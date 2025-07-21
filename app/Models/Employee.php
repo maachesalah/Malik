4bb8d634f2d4181895cbc760131ba81bcd4f31fd
maachesalah <41996340+maachesalah@users.noreply.github.com>
2025-07-22 00:36:09 +0100

    Update Employee.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace App\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'employees';
+
+    protected $fillable = [
+        'name', 'email', 'dob', 'salary', 'gender', 'active', 'img'
+    ];
+}
